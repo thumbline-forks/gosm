d0adefb6065de0dbafece24b73459c55c6d3e59a
thumbline <github.thumbline@gmail.com>
2017-09-30 20:49:07 -0400

    int > int64 minor edition

--- gosm.go
+++ gosm.go
@@ -1,13 +1,15 @@
 package gosm
+// Forked from github.com/apeyroux/gosm
+// In order to add necessary minor fixes
 
 import (
 	"math"
 )
 
 type Tile struct {
-	Z    int
-	X    int
-	Y    int
+	Z    int64
+	X    int64
+	Y    int64
 	Lat  float64
 	Long float64
 }
@@ -18,13 +20,13 @@ type BBox struct {
 }
 
 type Conversion interface {
-	Deg2num(t *Tile) (x int, y int)
+	Deg2num(t *Tile) (x int64, y int64)
 	Num2deg(t *Tile) (lat float64, long float64)
 }
 
-func (t *Tile) Deg2num() (x int, y int) {
-	x = int(math.Floor((t.Long + 180.0) / 360.0 * (math.Exp2(float64(t.Z)))))
-	y = int(math.Floor((1.0 - math.Log(math.Tan(t.Lat*math.Pi/180.0)+1.0/math.Cos(t.Lat*math.Pi/180.0))/math.Pi) / 2.0 * (math.Exp2(float64(t.Z)))))
+func (t *Tile) Deg2num() (x int64, y int64) {
+	x = int64(math.Floor((t.Long + 180.0) / 360.0 * (math.Exp2(float64(t.Z)))))
+	y = int64(math.Floor((1.0 - math.Log(math.Tan(t.Lat*math.Pi/180.0)+1.0/math.Cos(t.Lat*math.Pi/180.0))/math.Pi) / 2.0 * (math.Exp2(float64(t.Z)))))
 	return
 }
 
@@ -35,7 +37,7 @@ func (t *Tile) Num2deg() (lat float64, long float64) {
 	return lat, long
 }
 
-func NewTileWithLatLong(lat float64, long float64, z int) (t *Tile) {
+func NewTileWithLatLong(lat float64, long float64, z int64) (t *Tile) {
 	t = new(Tile)
 	t.Lat = lat
 	t.Long = long
@@ -44,7 +46,7 @@ func NewTileWithLatLong(lat float64, long float64, z int) (t *Tile) {
 	return
 }
 
-func NewTileWithXY(x int, y int, z int) (t *Tile) {
+func NewTileWithXY(x int64, y int64, z int64) (t *Tile) {
 	t = new(Tile)
 	t.Z = z
 	t.X = x
@@ -55,14 +57,13 @@ func NewTileWithXY(x int, y int, z int) (t *Tile) {
 
 func BBoxTiles(topTile Tile, bottomTile Tile) ([]*Tile, error) {
 	tiles := []*Tile{}
-	for _, z := range []int{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19} {
+	for _, z := range []int64{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19} {
 		tMax := NewTileWithLatLong(topTile.Lat, topTile.Long, z)
 		tMin := NewTileWithLatLong(bottomTile.Lat, bottomTile.Long, z)
 		//nbtiles := math.Abs((float64(tMax.X))-float64(tMin.X)) + math.Abs(float64(tMax.Y)-float64(tMin.Y))
 		for x := tMin.X; x <= tMax.X; x++ {
 			for y := tMax.Y; y <= tMin.Y; y++ {
-				tile := NewTileWithXY(x, y, z)
-				tiles = append(tiles, tile)
+				tiles = append(tiles, NewTileWithXY(x, y, z))
 			}
 		}
 	}
